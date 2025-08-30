38ce81867e812ccfe20168091ccb2c37b3d6b6b4
Tyfanny-eva02 <aleenico753@gmail.com>
2025-08-30 20:51:29 -0300

    a

--- GalinhaSurfers/Assets/scripts/barra/fome.cs
+++ GalinhaSurfers/Assets/scripts/barra/fome.cs
@@ -7,47 +7,57 @@ using UnityEngine.XR;
 
 public class fome : MonoBehaviour
 {
-    public Material materialRainbow; 
+    public Material materialRainbow;
     private Material materialOriginal;
     private Material materialOriginalBarra;
     public Renderer bFome;
     public SkinnedMeshRenderer personagemRenderer;
     public AudioSource ComidasSons;
+    public RectTransform topoFome;
 
-    private float delayAntesDeTUDO =8f;
+    private float delayAntesDeTUDO = 8f;
     private bool taLiberado = false;
+
     [Header("Barra Fome")]
     public RectTransform barraFome;
     public float valorAtualFome;
     public float valorMaxFome;
-    public float velFome; // velocidade normal de diminui��o da fome [Header("Barra Pimenta")] 
+    public float velFome; // velocidade normal de diminuição da fome
+
+    [Header("Barra Pimenta")]
     private float alturaInicialFome;
     private float valorMaxInicialFome;
     public RectTransform barraPimenta;
-    public float duracaoPimenta; [Range(0f, 1f)]
-    public float tamanhoPimenta; // 25% 
+    public float duracaoPimenta;
+    [Range(0f, 1f)]
+    public float tamanhoPimenta; // 25%
     private float valorMaxPimenta;
     private float valorAtualPimenta;
     private bool pimentaAtiva;
     private float valorMaxFomeAntesPimenta;
     private float debitoPimenta = 0f;
     public AudioClip pimentaSom;
+
     [Header("Cookie")]
     public float duracaoCookie;
     private bool cookieAtivo;
     private float tempoRestanteCookie;
     public AudioClip cookieSom;
+
     [Header("CogumeloMal")]
     public float duracaoCM;
     private bool CMAtivo;
     private float tempoRestanteCM;
+
     [Header("CogumeloMaluco")]
     public float duracaoCoguMaluco;
     public List<Sprite> todasFrutas;
+
     [Header("Escorpiao")]
     private bool escorpiaoAtivo;
     public float duracaoEscorpiao;
     private float tempoRestanteEscorpion;
+
     [Header("Morte")]
     public GameObject mortehud;
     public Pontos scriptPontuacao;
@@ -57,64 +67,84 @@ public class fome : MonoBehaviour
     public TMP_Text ScoreSombra;
     public SpawnScript spwanScript;
     public AudioSource MscHighScore;
+
+    public RectTransform meioContorno; // objeto que vai ser escalonado
+    public float alturaMaxMeio = 433f;
+    public float alturaMinMeio = 80f;
+
+    float valorMaxFomeBase = 925.5f;
+
     public void Start()
     {
         materialOriginalBarra = bFome.material;
         materialOriginal = personagemRenderer.material;
-        // Fome 
+
+        // Fome
         alturaInicialFome = barraFome.sizeDelta.y;
         valorMaxInicialFome = alturaInicialFome;
         valorMaxFome = valorMaxInicialFome;
         valorAtualFome = valorMaxFome;
-        // Pimenta 
+
+        // Pimenta
         barraPimenta.gameObject.SetActive(false);
         pimentaAtiva = false;
-        StartCoroutine(Perai());
 
+        StartCoroutine(Perai());
     }
-    private IEnumerator Perai() 
-    { 
-        yield return new WaitForSeconds(delayAntesDeTUDO); 
-        taLiberado = true; 
+
+    private IEnumerator Perai()
+    {
+        yield return new WaitForSeconds(delayAntesDeTUDO);
+        taLiberado = true;
     }
 
     void Update()
     {
         if (!taLiberado) return;
 
+        if (Input.GetKeyDown(KeyCode.E))
+            AtivarPimenta();
 
         AtualizarFome();
+        AtualizarTopoFome();
+        AtualizarMeioContorno();
         AtualizarPimenta();
         AtualizarCookie();
         AtualizarEscorpion();
         AtualizarCM();
-        if (pimentaAtiva) AtualizarPosicaoPimenta();
-        if (valorAtualFome <= 0 && !Morreu)
-        {
-            StartCoroutine(GalinhaMorreu());
-        }
 
+        if (pimentaAtiva)
+            AtualizarPosicaoPimenta();
+
+        if (valorAtualFome <= 0 && !Morreu)
+            StartCoroutine(GalinhaMorreu());
     }
+
     void AtualizarFome()
     {
-        valorAtualFome -= velFome * Time.deltaTime; valorAtualFome = Mathf.Clamp(valorAtualFome, 0, valorMaxFome);
+        valorAtualFome -= velFome * Time.deltaTime;
+        valorAtualFome = Mathf.Clamp(valorAtualFome, 0, valorMaxFome);
+
         float alturaMaximaFome = alturaInicialFome * (valorMaxFome / valorMaxInicialFome);
-        float porcentagemFome = valorAtualFome / valorMaxFome; barraFome.sizeDelta = new Vector2(barraFome.sizeDelta.x, alturaMaximaFome * porcentagemFome);
+        float porcentagemFome = valorAtualFome / valorMaxFome;
 
-
+        barraFome.sizeDelta = new Vector2(barraFome.sizeDelta.x, alturaMaximaFome * porcentagemFome);
     }
+
     void AtualizarPimenta()
     {
         if (!pimentaAtiva) return;
-        // Redu��o da pimenta
+
+        // Redução da pimenta
         float velPimenta = valorMaxPimenta / duracaoPimenta;
         valorAtualPimenta -= velPimenta * Time.deltaTime;
         valorAtualPimenta = Mathf.Clamp(valorAtualPimenta, 0, valorMaxPimenta);
-        // Atualiza altura da barra da pimenta 
+
+        // Atualiza altura da barra da pimenta
         float alturaPimenta = alturaInicialFome * (valorMaxPimenta / valorMaxInicialFome);
         float porcentagemPimenta = valorAtualPimenta / valorMaxPimenta;
         barraPimenta.sizeDelta = new Vector2(barraPimenta.sizeDelta.x, alturaPimenta * porcentagemPimenta);
-        //Debug.Log($"[PIMENTA] Max: {valorMaxPimenta:F2} | Atual: {valorAtualPimenta:F2}");
+
         if (valorAtualPimenta <= 0f)
         {
             pimentaAtiva = false;
@@ -122,69 +152,75 @@ public class fome : MonoBehaviour
             valorMaxFome = valorMaxFomeAntesPimenta;
             valorMaxFome += debitoPimenta * (1 / tamanhoPimenta);
             debitoPimenta = 0f;
-            if (valorMaxFome >= 1000) valorMaxFome = 1000;
+
+            if (valorMaxFome >= 925.5) valorMaxFome = 925.5f;
             if (valorMaxFome <= 0) valorMaxFome = 0;
+
             scriptPontuacao.pimentaMenosSpeed();
         }
     }
+
     void AtualizarPosicaoPimenta()
     {
-        float fomeTotal = valorMaxFome + valorMaxPimenta;
+        float topoMinY = -245f;
+        float topoMaxY = 462.3f;
+        float minFome = 230f;
+        float maxFome = 925.5f;
 
-        float posY = (fomeTotal - 462.75f) * alturaInicialFome / 925.5f;
+        float t = Mathf.InverseLerp(minFome, maxFome, valorMaxFomeBase);
+        float posY = Mathf.Lerp(topoMinY, topoMaxY, t);
 
         Vector3 posPimenta = barraPimenta.localPosition;
         posPimenta.y = posY;
-        if (posPimenta.y > (alturaInicialFome / 2f))
-            posPimenta.y = alturaInicialFome / 2f;
-
-        if (posPimenta.y < -(alturaInicialFome / 2f))
-            posPimenta.y = -(alturaInicialFome / 2f);
         barraPimenta.localPosition = posPimenta;
     }
+
     void AtualizarCookie()
     {
-        if (!cookieAtivo)
-            return;
+        if (!cookieAtivo) return;
+
         valorAtualFome += 200f * Time.deltaTime;
         tempoRestanteCookie -= Time.deltaTime;
+
         if (tempoRestanteCookie <= 0f)
         {
             bFome.material = materialOriginalBarra;
             personagemRenderer.material = materialOriginal;
             cookieAtivo = false;
             scriptPontuacao.cookieMenosSpeed();
-            
         }
     }
+
     void AtualizarEscorpion()
     {
-        if(!escorpiaoAtivo)
-            return ;
+        if (!escorpiaoAtivo) return;
+
         tempoRestanteEscorpion -= Time.deltaTime;
+
         if (tempoRestanteEscorpion <= 0f)
         {
             escorpiaoAtivo = false;
             scriptPontuacao.escorpiaoMenosSpeed();
         }
     }
+
     void AtualizarCM()
     {
-        if (!CMAtivo)
-            return;
-       
+        if (!CMAtivo) return;
+
         valorAtualFome += 150f * Time.deltaTime;
         tempoRestanteCM -= Time.deltaTime;
+
         if (tempoRestanteCM <= 0f)
-        {
             CMAtivo = false;
-        }
     }
+
     public void AtivarPimenta()
     {
         pimentaAtiva = true;
         ComidasSons.clip = pimentaSom;
         ComidasSons.Play();
+
         if (!barraPimenta.gameObject.activeSelf)
             barraPimenta.gameObject.SetActive(true);
 
@@ -192,9 +228,9 @@ public class fome : MonoBehaviour
         {
             // Resetar o tamanho e duração da pimenta
             valorAtualPimenta = valorMaxPimenta;
-
             float alturaPimenta2 = alturaInicialFome * (valorMaxPimenta / valorMaxInicialFome);
             barraPimenta.sizeDelta = new Vector2(barraPimenta.sizeDelta.x, alturaPimenta2);
+            // **Não atualizar posição ainda**
         }
         else
         {
@@ -203,69 +239,72 @@ public class fome : MonoBehaviour
 
             valorMaxPimenta = valorMaxFomeAntesPimenta * tamanhoPimenta;
             valorAtualPimenta = valorMaxPimenta;
-
             valorMaxFome -= valorMaxPimenta;
 
             float alturaPimenta = alturaInicialFome * (valorMaxPimenta / valorMaxInicialFome);
             barraPimenta.sizeDelta = new Vector2(barraPimenta.sizeDelta.x, alturaPimenta);
 
+            // Agora sim atualizar a posição corretamente
             AtualizarPosicaoPimenta();
         }
-
     }
+
     public void AtivarCookie()
     {
         bFome.material = materialRainbow;
         personagemRenderer.material = materialRainbow;
         ComidasSons.clip = cookieSom;
         ComidasSons.Play();
-        if (!cookieAtivo)
-            scriptPontuacao.cookieSpeed();
+
+        if (!cookieAtivo) scriptPontuacao.cookieSpeed();
+
         cookieAtivo = true;
         tempoRestanteCookie = duracaoCookie;
         AtualizarCookie();
     }
+
     public void AtivarCogumeloMal()
     {
         CMAtivo = true;
         tempoRestanteCM = duracaoCM;
-        valorMaxFome = valorMaxFome / 4;
+        valorMaxFome = valorMaxFome / 2;
     }
+
     public void AtivarEscorpiaoLentidao()
     {
         if (!escorpiaoAtivo)
             scriptPontuacao.escorpiaoSpeed();
+
         escorpiaoAtivo = true;
         tempoRestanteEscorpion = duracaoEscorpiao;
-
     }
+
     public void AdicionarFome(float quantidade)
     {
         valorAtualFome += quantidade;
         valorAtualFome = Mathf.Clamp(valorAtualFome, 0, valorMaxFome);
     }
+
     public void AlterarMaxFome(float quantidade)
     {
         if (pimentaAtiva)
         {
-            // Divide a altera��o entre fome e pimenta
+            // Divide a alteração entre fome e pimenta
             float partePimenta = quantidade * tamanhoPimenta;
             float parteFome = quantidade * (1 - tamanhoPimenta);
 
             // Atualiza limites
             valorMaxFome += parteFome;
-            valorMaxFome = Mathf.Clamp(valorMaxFome, 1, 1000);
-
+            valorMaxFome = Mathf.Clamp(valorMaxFome, 1, 925.5f);
             valorMaxPimenta += partePimenta;
-            valorMaxPimenta = Mathf.Clamp(valorMaxPimenta, 0, 1000);
+            valorMaxPimenta = Mathf.Clamp(valorMaxPimenta, 0, 925.5f);
 
             // Atualiza valores atuais
             valorAtualPimenta += partePimenta;
             valorAtualPimenta = Mathf.Clamp(valorAtualPimenta, 0, valorMaxPimenta);
-
             valorAtualFome = Mathf.Clamp(valorAtualFome, 0, valorMaxFome);
 
-            // D�bito da pimenta
+            // Débito da pimenta
             debitoPimenta += partePimenta;
 
             // Reposiciona a barra da pimenta
@@ -273,13 +312,15 @@ public class fome : MonoBehaviour
         }
         else
         {
-            // S� fome mesmo
+            // Só fome mesmo
             valorMaxFome += quantidade;
-            valorMaxFome = Mathf.Clamp(valorMaxFome, 1, 1000);
+            valorMaxFome = Mathf.Clamp(valorMaxFome, 1, 925.5f);
             valorAtualFome = Mathf.Clamp(valorAtualFome, 0, valorMaxFome);
         }
     }
+
     private Coroutine cogumeloCoroutine;
+
     public void AtivarCogumeloMaluco()
     {
         if (cogumeloCoroutine != null)
@@ -289,6 +330,7 @@ public class fome : MonoBehaviour
 
         cogumeloCoroutine = StartCoroutine(EfeitoAlucinogeno(duracaoCoguMaluco));
     }
+
     private IEnumerator EfeitoAlucinogeno(float duracao)
     {
         float tempoPassado = 0f;
@@ -301,6 +343,7 @@ public class fome : MonoBehaviour
             foreach (GameObject fruta in frutas)
             {
                 if (fruta == null) continue;
+
                 SpriteRenderer sr = fruta.GetComponent<SpriteRenderer>();
                 if (sr == null || todasFrutas.Count == 0) continue;
                 if (!originais.ContainsKey(fruta))
@@ -333,34 +376,83 @@ public class fome : MonoBehaviour
             if (item.Key != null)
             {
                 SpriteRenderer sr = item.Key.GetComponent<SpriteRenderer>();
-                if (sr != null)
-                    sr.sprite = item.Value;
+                if (sr != null) sr.sprite = item.Value;
             }
         }
 
         cogumeloCoroutine = null;
         Debug.Log("Efeito alucinógeno passou!");
     }
+
     private IEnumerator GalinhaMorreu()
     {
-        int recorde = PlayerPrefs.GetInt("pontuacao",0);
-        Debug.Log(recorde);
+        int recorde = PlayerPrefs.GetInt("pontuacao", 0);
         Morreu = true;
         scriptPontuacao.galinhaMorta = true;
         spwanScript.morreu = true;
+
         yield return new WaitForSeconds(3.5f);
         mortehud.SetActive(true);
+
         Score.text = "Score:" + scriptPontuacao.distanciaNum;
         ScoreSombra.text = Score.text;
+
         int pontosAtuais = scriptPontuacao.distanciaNum;
         if (pontosAtuais > recorde)
         {
-            Debug.Log(pontosAtuais);
             PlayerPrefs.SetInt("pontuacao", pontosAtuais);
             yield return new WaitForSeconds(3f);
             MscHighScore.Play();
             HighScore.SetActive(true);
         }
-        //Usado para atualizar o score em breve
+    }
+
+    void AtualizarMeioContorno()
+    {
+        float minFome = 230f;
+        float maxFome = 925.5f;
+        float novaAltura;
+
+        // usa valorMaxFomeBase para ignorar a pimenta
+        float valorParaCalculo = Mathf.Clamp(valorMaxFomeBase, minFome, maxFome);
+
+        if (valorParaCalculo <= minFome)
+            novaAltura = alturaMinMeio;
+        else if (valorParaCalculo >= maxFome)
+            novaAltura = alturaMaxMeio;
+        else
+            novaAltura = alturaMinMeio + (valorParaCalculo - minFome) * (alturaMaxMeio - alturaMinMeio) / (maxFome - minFome);
+
+        Vector2 size = meioContorno.sizeDelta;
+        size.y = novaAltura;
+        meioContorno.sizeDelta = size;
+    }
+
+    void AtualizarTopoFome()
+    {
+        float topoMinY = -245f;
+        float topoMaxY = 446.3f;
+        float minFome = 230f;
+        float maxFome = 925.5f;
+        float ajusteVisual = 14f;
+
+        // usa valorMaxFomeBase para ignorar a pimenta
+        float valorParaCalculo = Mathf.Clamp(valorMaxFomeBase, minFome, maxFome);
+        float posY;
+
+        if (valorParaCalculo <= minFome)
+            posY = topoMinY;
+        else if (valorParaCalculo >= maxFome)
+            posY = topoMaxY;
+        else
+        {
+            posY = topoMinY + (valorParaCalculo - minFome) * (topoMaxY - topoMinY) / (maxFome - minFome);
+            posY -= ajusteVisual;
+        }
+
+        // aplica no topo
+        Vector3 pos = topoFome.localPosition;
+        pos.y = posY;
+        topoFome.localPosition = pos;
     }
 }
