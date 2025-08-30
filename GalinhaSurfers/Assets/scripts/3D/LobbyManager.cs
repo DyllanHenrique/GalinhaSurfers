ed1ed71f28b5b5c5a3b775ce8bf984475a31fb5b
akkazyn <83934518+akkazyn@users.noreply.github.com>
2025-08-30 00:04:37 -0300

    Revert "vc é muito ******"
    This reverts commit a4ba7bfbddfc632a56b528fcb55d80a70d46388e.

--- GalinhaSurfers/Assets/scripts/3D/LobbyManager.cs
+++ GalinhaSurfers/Assets/scripts/3D/LobbyManager.cs
@@ -15,8 +15,6 @@ public class LobbyManager : MonoBehaviour
 
     public GameObject startObject;
 
-    public ParticleSystem penasPartic;
-
     //MenuOpcoes
     public GameObject optionsMenu;
 
@@ -37,8 +35,6 @@ public class LobbyManager : MonoBehaviour
         startObject.SetActive(true);
 
         smokeObject.SetActive(false);
-
-        penasPartic.Stop();
 
 
         Debug.Log("Idle");
@@ -75,8 +71,7 @@ public class LobbyManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isTransitioning
-            && !optionsMenu.activeSelf && !htpMenu.activeSelf)
+        if (Input.GetKeyDown(KeyCode.Space) && !isTransitioning)
         {
             StartCoroutine(TransitionToGame());
         }
@@ -115,8 +110,6 @@ public class LobbyManager : MonoBehaviour
         startObject.SetActive(false);
 
         smokeObject.SetActive(true);
-
-        penasPartic.Play();
 
         yield return new WaitForSeconds(0.2f);
 
